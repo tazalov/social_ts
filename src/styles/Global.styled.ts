408fe371cae55dd8ccae637bc5f10a561eed96b5
Iliya Tazalov <134263398+tazalov@users.noreply.github.com>
2023-07-23 14:36:09 +0300

    Install prettier, sc, create Theme, GlobalStyles, update .gitignore

---
+++ src/styles/Global.styled.ts
@@ -0,0 +1,67 @@
+import { createGlobalStyle } from "styled-components";
+import { theme } from "./Theme";
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    border: 0;
+  }
+  :focus,
+  :active {
+    outline: none;
+  }
+  a:focus,
+  a:active {
+    outline: none;
+  }
+  
+  body {
+    line-height: 1;
+    font-family: 'Rubik', -apple-system, BlinkMacSystemFont, 'Arial', sans-serif;
+    //text-rendering: optimizeibility;
+    -ms-text-size-adjust: 100%;
+    -moz-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-fontoothing: grayscale;
+    background-color: ${theme.colors.primaryBg};
+    color: ${theme.colors.primaryFont};
+  }
+  input,
+  button,
+  textarea {
+    font-size: inherit;
+  }
+  button {
+    cursor: pointer;
+    color: inherit;
+    background-color: inherit;
+  }
+  a {
+    color: inherit;
+  }
+  a:link,
+  a:visited {
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: none  }
+  ul li {
+    list-style: none;
+  }
+  img {
+    vertical-align: top;
+  }
+  h1,
+  h2  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: inherit;
+    font-size: inherit;
+  }
+`;
---
+++ src/styles/Theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primaryBg: "#49526f",
+    secondaryBg: "#161828",
+
+    accent: "#00b7ff",
+
+    primaryFont: "#FFFFFF",
+    secondaryFont: "#808080",
+  },
+  media: {
+    tablet: "screen and (max-width: 768px)",
+    mobile: "screen and (max-width: 576px)",
+  },
+};
