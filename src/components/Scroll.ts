58bfdd17b5d11857bc27916f8e0d7edf175b0ae0
tazalov <tazalov9@gmail.com>
2023-07-26 20:19:24 +0300

    Create custom scroll styles

---
+++ src/components/Scroll.ts
@@ -0,0 +1,17 @@
+import { css } from "styled-components";
+import { theme } from "../styles/Theme";
+
+export const Scroll = css`
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: ${theme.colors.thirdBg};
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${theme.colors.primaryBg};
+    border-radius: 4px;
+  }
+`;
