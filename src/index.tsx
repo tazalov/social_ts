f17980828391aaa92568c47946100effae4ae4a7
tazalov <tazalov9@gmail.com>
2023-07-30 11:28:32 +0300

    Delete function renderThree

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,10 @@ import { GlobalStyle } from "./styles/Global.styled";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
-const renderThree = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <GlobalStyle />
-      <App state={store.getState()} dispatch={store.dispatch.bind(store)} />
-    </Provider>,
-    document.getElementById("root"),
-  );
-};
-renderThree();
-store.subscribe(renderThree);
+ReactDOM.render(
+  <Provider store={store}>
+    <GlobalStyle />
+    <App state={store.getState()} dispatch={store.dispatch.bind(store)} />
+  </Provider>,
+  document.getElementById("root"),
+);
